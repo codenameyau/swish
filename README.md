7c24d7f425ddeb76905033fc8d6b483240278cc5
Jorge Yau <jyau01@citymail.cuny.edu>
2014-06-23 23:08:19 -0400

    Updating prototype add

--- README.md
+++ README.md
@@ -2,3 +2,15 @@ swish
 =====
 
 Three.js basketball game
+
+
+##ThreeGen Game Engine Documentation
+
+###Collision
+* 0 -> No collision with any objects
+* 1 -> Collide with everything except for 0 or 1
+* 2 -> Collide with everything except for 0 or 2
+* 3 -> Collide with everything except for 0 or 3
+* ...
+* n -> Collide with everything except for 0 or n
+
--- app/assets/js/lib/plugins/ThreeGen.js
+++ app/assets/js/lib/plugins/ThreeGen.js
@@ -1,4 +1,7 @@
-/* exported THREEGEN */
+/*-------JSHint Directives-------*/
+/* global THREE                  */
+/* exported THREEGEN             */
+/*-------------------------------*/
 'use strict';
 
 
@@ -8,8 +11,9 @@ function GameEngine() {
  /**************************
   * GameEngine: Properties *
   **************************/
-  this.gravity  = -10;
   this.entities = {};
+  this.gravity  = -10;
+  this.entityCount = 0;
 
  /*******************************
   * GameEngine: Private Methods *
@@ -23,16 +27,28 @@ function GameEngine() {
 GameEngine.prototype.update = function() {
 };
 
-GameEngine.prototype.add = function(object) {
-  var objectID = this.generateID();
+
+
+GameEngine.prototype.add = function(object, setCollision, setVelocity) {
+  // Adds object to game engine entities
+  setCollision = setCollision || 1;
+  setVelocity  = setVelocity  || {x: 0, y: 0, z: 0};
+
+  // Create entity with incremental ID
+  var objectID = this.entityCount;
+  this.entityCount += 1;
+
+  // Define entity properties
+  this.entities[objectID] = {
+    mesh : object,
+    velocity : new THREE.Vector3(setVelocity.x, setVelocity.y, setVelocity.z),
+    collision : setCollision,
+  };
+
+  console.log(this.entities);
 
 };
 
-
-GameEngine.prototype.generateID = function() {
-  // Generates a random base 36 string
-  return Math.random().toString(36).substring(2);
-};
 
 
 // Export: THREEGEN
